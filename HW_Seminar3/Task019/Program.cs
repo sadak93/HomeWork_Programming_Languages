98792dc1b222f0bb5f08148b2164082c8fc240f5
Sadak <s.sadak02@gmail.com>
2023-05-29 19:07:29 +0600

    Палиндром или нет 5значное число

--- HW_Seminar3/Task019/Program.cs
+++ HW_Seminar3/Task019/Program.cs
@@ -4,15 +4,42 @@
 // 12821 -> да
 // 23432 -> да
 
+void Palindrom(int num)
+{
+    int num5razryad = num / 10000,
+        num4razryad = (num / 1000) % 10,
+        num2razryad = (num % 100) / 10,
+        num1razryad = num % 10;
+
+    if (num5razryad == num1razryad && num4razryad == num2razryad)
+        System.Console.WriteLine("yes");
+    else
+        System.Console.WriteLine("no");
+}
+
 Console.Clear();
 System.Console.Write("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int num5razryad=num / 10000, 
-    num4razryad=(num / 1000) % 10,
-    num2razryad=(num % 100) / 10,
-    num1razryad=num % 10;
 
-if (num5razryad == num1razryad && num4razryad == num2razryad)
-    System.Console.WriteLine("yes");
-else
-    System.Console.WriteLine("no");
+Palindrom(num);
+
+// string Palindrom1(int num)
+// {
+//     string result = "";
+//     int num5razryad = num / 10000,
+//         num4razryad = (num / 1000) % 10,
+//         num2razryad = (num % 100) / 10,
+//         num1razryad = num % 10;
+
+//     if (num5razryad == num1razryad && num4razryad == num2razryad)
+//         result = "yes";
+//     else
+//         result = "no";
+//     return result;
+// }
+
+// Console.Clear();
+// System.Console.Write("Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(Palindrom1(num));
