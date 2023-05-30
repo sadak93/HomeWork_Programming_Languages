c00a5ed9ba763730cbbe7755f0ecc15cb9c78421
Sadak <s.sadak02@gmail.com>
2023-05-30 20:18:15 +0600

    Палиндром любой разрядности

---
+++ HW_Seminar3/Task019_Hard/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19 - HARD необязательная
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, 
+// является ли оно палиндромом. Через строку нельзя решать само собой.
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1=num;
+int revnum=0;
+int div = 0;
+while (num1>0)
+{
+    div=num1%10;
+    revnum=revnum*10+div;
+    num1=num1/10;
+}
+if (num==revnum)
+    System.Console.WriteLine("Число является палиндромом");
+else
+    System.Console.WriteLine("Число не является палиндромом");
