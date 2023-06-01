fe742ff1cfb46b982da5f738a8b756715e98aa9d
Sadak <s.sadak02@gmail.com>
2023-06-01 17:04:00 +0600

    Вывод введенного массива

---
+++ HW_Seminar4/Task029/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Massiv(int[] Array)
+{
+    Console.WriteLine("Введите числа:");
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"Число {i + 1}: ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+        
+    }
+     System.Console.WriteLine(String.Join(" ", Array));
+}
+Console.Clear();
+int[] Array = new int[8];
+Massiv(Array);
+
+// Console.Clear();
+// int[] Array = new int[8];
+// Console.WriteLine("Введите числа:");
+// for (int i = 0; i < 8; i++)
+// {
+//     Console.Write($"Число {i + 1}: ");
+//     Array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// System.Console.WriteLine(String.Join(" ", Array));
