344852f4a52d2213666c21724a5ee4d0668fd90c
Sadak <s.sadak02@gmail.com>
2023-06-01 15:44:50 +0600

    Возведение числа А в степень В

---
+++ HW_Seminar4/Task025/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Stepen (int A, int B)
+{
+    int result = 0;
+    result = Convert.ToInt32(Math.Pow(A,B));
+    return result;
+    
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите число которое нужно возвести в степень: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Полученный результат: {Stepen(A,B)}");
+
+
+// void Stepen (int A, int B)
+// {
+//     int result = Convert.ToInt32(Math.Pow(A,B));
+//     System.Console.WriteLine($"Полученный результат: {result}");
+// }
+
+// Console.Clear();
+// System.Console.WriteLine("Введите число которое нужно возвести в степень: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите степень: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// Stepen(A,B);
