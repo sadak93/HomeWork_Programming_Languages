a131024ef1d945513591de52a2b16a8cf114a575
Sadak <s.sadak02@gmail.com>
2023-06-06 18:58:19 +0600

    Четное число в массиве из 3хзначных чисел

---
+++ HW_Seminar5/Task034/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void FillArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    }
+}
+void PrintArray (int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+void ChetnieChisla (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0)
+            System.Console.WriteLine($"Четные числа в массиве: {array[i]}");
+    }
+}
+Console.Clear();
+int[] array = new int[3];
+FillArray(array);
+PrintArray(array);
+ChetnieChisla(array);
