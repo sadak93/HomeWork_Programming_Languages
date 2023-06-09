53b18e0c1afe38708560cccc639e59565298e495
Sadak <s.sadak02@gmail.com>
2023-06-09 21:45:53 +0600

    Нахождение матем параметров

--- HW_Seminar5/Task_Hard_Stat/Program.cs
+++ HW_Seminar5/Task_Hard_Stat/Program.cs
@@ -36,48 +36,93 @@ int MaxNum (int [] array)
     return max;
 }
 
-void MathParametr(int[] array)
+int IndexMaxNum (int [] array)
 {
     int max = array[0];
     int imax = 0;
-    int min = array [0];
-    int imin = 0;
-    double sum = 0;
-    double average = 0;
-    for (int i = 0; i < array.Length; i++)
+     for (int i = 0; i < array.Length; i++)
     {
         if (array[i]>max)
         {    
             max = array[i];
             imax = i;
         }
+    }
+    return imax;
+}
+
+int MinNum (int [] array)
+{
+    int min = array [0];
+    int imin = 0;
+     for (int i = 0; i < array.Length; i++)
+    {
         if (array[i]<min)
         {
             min = array[i];
             imin = i;
         }
-        sum = Convert.ToDouble(sum + array[i]);
-        average = Convert.ToDouble(sum/array.Length);
-
     }
-    System.Console.WriteLine(max);
-    System.Console.WriteLine(imax);
-    System.Console.WriteLine(min);
-    System.Console.WriteLine(imin);
-    System.Console.WriteLine(sum);
-    System.Console.WriteLine(average);
+    return min;
 }
 
-void Mediana (int[] array)
+int IndexMinNum (int [] array)
 {
+    int min = array [0];
+    int imin = 0;
+     for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]<min)
+        {
+            min = array[i];
+            imin = i;
+        }
+    }
+    return imin;
+}
+
+double Average (int [] array)
+{
+    double sum = 0;
+    double average = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = Convert.ToDouble(sum + array[i]);
+        average = Convert.ToDouble(sum/array.Length);
+    }
+    return average;
+}
+
+int [] SortArray (int [] array2)
+{
+    for (int i = 0; i < array2.Length-1; i++)
+    {
+        int imin = i;
+        for (int j = i+1; j < array2.Length; j++)
+        {
+            if (array2[j]<array2[imin])
+                imin = j;
+        }
+        int temp = array2[i];
+        array2[i]=array2[imin];
+        array2[imin]=temp;
+    }
+    return array2;
+}
+
+void Mediana (int[] array2)
+{
+    
     double mediana = 0;
     double sumMedian = 0;
-    if (array.Length%2==1)
-        mediana = array[array.Length/2];
+    if (array2.Length%2==1)
+        mediana = array2[array2.Length/2];
     else
-        sumMedian = Convert.ToDouble(array[array.Length/2]+array[array.Length/2-1]);
-        mediana = Convert.ToDouble(sumMedian/2);   
-    System.Console.WriteLine(mediana);
+        {
+            sumMedian = Convert.ToDouble(array2[array2.Length/2]+array2[array2.Length/2-1]);
+            mediana = Convert.ToDouble(sumMedian/2);
+        }   
+    System.Console.WriteLine($"Медианное значения массива: {mediana}");
 }
 
 Console.Clear();
@@ -88,7 +133,45 @@ int[] array2 = array;
 
 FillArray(array);
 PrintArray(array);
-MathParametr(array);
-Mediana(array);
-PrintArray(array2);
-System.Console.WriteLine($"{MaxNum(array)}");
+
+System.Console.WriteLine($"Максимальное значение: {MaxNum(array)}");
+System.Console.WriteLine($"Индекс макс значения: {IndexMaxNum(array)}");
+System.Console.WriteLine($"Минимальное значение: {MinNum(array)}");
+System.Console.WriteLine($"Индекс мин значения: {IndexMinNum(array)}");
+System.Console.WriteLine($"Среднее арифметическое значение:{Average(array)}");
+PrintArray(SortArray(array));
+Mediana(array2);
+
+
+
+// void MathParametr(int[] array)
+// {
+//     int max = array[0];
+//     int imax = 0;
+//     int min = array [0];
+//     int imin = 0;
+//     double sum = 0;
+//     double average = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i]>max)
+//         {    
+//             max = array[i];
+//             imax = i;
+//         }
+//         if (array[i]<min)
+//         {
+//             min = array[i];
+//             imin = i;
+//         }
+//         sum = Convert.ToDouble(sum + array[i]);
+//         average = Convert.ToDouble(sum/array.Length);
+
+//     }
+//     System.Console.WriteLine(max);
+//     System.Console.WriteLine(imax);
+//     System.Console.WriteLine(min);
+//     System.Console.WriteLine(imin);
+//     System.Console.WriteLine(sum);
+//     System.Console.WriteLine(average);
+// }
