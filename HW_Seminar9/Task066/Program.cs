c53742b96c2553a14345ad1f2e065ad1975c976f
Sadak <s.sadak02@gmail.com>
2023-06-20 16:09:27 +0600

    Сумма чисел от M до N

---
+++ HW_Seminar9/Task066/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SummNumbers(int num1, int num2)
+{
+    int summ = 0;
+    while (true)
+    {
+        summ = summ + num2;
+        num2--;
+        if (num2 == num1-1)
+            break;
+    }
+    return summ;
+}
+int SummNumbersRec(int num1, int num2)
+{
+    if (num2==num1)  return num1;
+    return SummNumbersRec(num1, num2-1) + num2;
+}
+Console.Clear();
+System.Console.WriteLine("Введите целое число начало: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите целое число конец: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма от {num1} до {num2} равна {SummNumbers(num1, num2)}");
+System.Console.WriteLine($"Сумма от {num1} до {num2} равна {SummNumbersRec(num1, num2)}");
