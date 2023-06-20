2f48707336262b366320ca3318101b11122d8315
Sadak <s.sadak02@gmail.com>
2023-06-20 15:46:24 +0600

    числа в промежутке от N до 1

---
+++ HW_Seminar9/Task064/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string ShowNumbers (int n)
+{
+    string str = "";
+    while (true)
+    {
+        str = str + ", " + Convert.ToString(n);
+        n--;
+        if (n == 0)
+            break;
+    }
+    return str;
+}
+
+string ShowNumbers2(int n)
+{
+    if (n == 1) return "1";
+    return $"{n}, " + ShowNumbers2(n-1);
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Натуральные числа в промежутке от N до 1: {ShowNumbers(n)}");
+System.Console.WriteLine($"Натуральные числа в промежутке от N до 1: {ShowNumbers2(n)}");
